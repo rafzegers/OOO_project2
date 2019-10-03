18cec97a124defb53592c9ba25edc93f3e3edc06
YankoBuyens <r0750784@student.ucll.be>
2019-10-03 18:08:17 +0200

    Iets of wat van Main gemaakt

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,8 +1,24 @@
 package ui;
 
+import domain.Caesar;
+import domain.Context;
+import domain.Spiegel;
+
 public class Main {
 
     public static void main(String[] args){
-        launch args;
+
+        Context spiegelCodeer = new Context("Hallo dit is een van de testen");
+        Context spiegeldecodeer = new Context("an tein siuht tid teod");
+        Context caeserCodeer = new Context("Hallo dit is een van de testen");
+
+        spiegelCodeer.setStrategy(new Spiegel());
+        spiegelCodeer.setStrategy(new Spiegel());
+        spiegelCodeer.setStrategy(new Caesar());
+
+
+        System.out.println(spiegelCodeer.encrypteer());
+        System.out.println(spiegeldecodeer.decrypteer());
+        System.out.println(caeserCodeer.encrypteer());
     }
 }
