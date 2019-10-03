5f6d4ba1e37ac8e4ae05d739864a1bc24aeb31f9
rafzegers <raf.zegers@student.ucll.be>
2019-10-03 18:07:32 +0200

    context gemaakt

--- src/domain/Context.java
+++ src/domain/Context.java
@@ -1,4 +1,35 @@
 package domain;
 
 public class Context {
+    private Strategy strategy;
+    private String tekst;
+
+
+    public Context(String tekst){
+        this.tekst = tekst;
+    }
+
+    public void setStrategy(Strategy strategy){
+        this.strategy = strategy;
+    }
+
+    public Strategy getStrategy(){
+        return strategy;
+    }
+
+    public String encrypteer(){
+        return   strategy.encrypteer(tekst);
+    }
+
+    public String decrypteer(){
+        return   strategy.decrypteer(tekst);
+    }
+
+    public String getTekst() {
+        return tekst;
+    }
+
+    public void setTekst(String tekst) {
+        this.tekst = tekst;
+    }
 }
